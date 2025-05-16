495ba2923238b814970375fb6870c2a9d827f6e5
isuyun <sylee@islab.snu.ac.kr>
2025-05-16 18:27:08 +0900

    [RUN] piper5hz_subtask_dataset_builder.py

--- piper5hz_subtask/piper5hz_subtask_dataset_builder.py
+++ piper5hz_subtask/piper5hz_subtask_dataset_builder.py
@@ -108,10 +108,10 @@ class Piper5HZ_subtask(tfds.core.GeneratorBasedBuilder):
 
         def _parse_example(episode_path):
             # load raw data --> this should change for your dataset
-            #data = np.load(episode_path, allow_pickle=True)     # this is a list of dicts in our case
+            data = np.load(episode_path, allow_pickle=True)     # this is a list of dicts in our case
 
-            with open(episode_path, "rb") as f:
-                data = pickle.load(f)
+        #    with open(episode_path, "rb") as f:
+        #        data = pickle.load(f)
 
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
             episode = []
