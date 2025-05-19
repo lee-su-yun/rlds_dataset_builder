bff31065eb79ab4a4a8d0a244c1de326a5d6fb2f
isuyun <sylee@islab.snu.ac.kr>
2025-05-19 13:52:03 +0900

    [RUN] piper5hz_subtask_dataset_builder.py

--- piper5hz_subtask/piper5hz_subtask_dataset_builder.py
+++ piper5hz_subtask/piper5hz_subtask_dataset_builder.py
@@ -10,14 +10,10 @@ import tensorflow_hub as hub
 class Piper5HZ_subtask(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for example dataset."""
 
-    VERSION = tfds.core.Version('1.0.0')
+    VERSION = tfds.core.Version('2.0.0')
     RELEASE_NOTES = {
       '1.0.0': 'Initial release.',
-      '1.5.0': '5hz.',
       '2.0.0': 'train',
-      '2.5.0': 'episode_id',
-        '3.0.0': 'add dataset',
-        '3.5.0': '20 episodes'
 
     }
 
@@ -86,7 +82,7 @@ class Piper5HZ_subtask(tfds.core.GeneratorBasedBuilder):
                 }),
                 'episode_metadata': tfds.features.FeaturesDict({
                     'file_path': tfds.features.Text(
-                        doc='/sdb1/piper_5hz/train'
+                        doc='/sdb1/piper_5hz_subtask/train'
                     ),
                     'episode_id': tfds.features.Text(
                         doc='episode_id'
@@ -97,7 +93,7 @@ class Piper5HZ_subtask(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Define data splits."""
         return {
-            'train': self._generate_examples(path='/sdb1/piper_subtask_data/train/pick/Pick the blue plastic cup in the center.'),
+            'train': self._generate_examples(path='/sdb1/piper_subtask_data/train/pick/'),
             # 'val': self._generate_examples(path='/sdb1/piper_5hz/validation'),
 
         }
@@ -152,7 +148,7 @@ class Piper5HZ_subtask(tfds.core.GeneratorBasedBuilder):
             return episode_path, sample
 
         # create list of all examples
-        episode_paths = glob.glob(f"{path}/*/episode.pickle")
+        episode_paths = glob.glob(f"{path}/*/*/episode.pickle")
         #episode_paths = episode_paths[:20]
 
         # for smallish datasets, use single-thread parsing
