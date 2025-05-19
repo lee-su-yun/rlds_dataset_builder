20d717e172a1a07adc0edb0b46d7415d5aca5593
isuyun <sylee@islab.snu.ac.kr>
2025-05-19 10:53:37 +0900

    [RUN] piper5hz_subtask_dataset_builder.py

--- piper5hz_subtask/piper5hz_subtask_dataset_builder.py
+++ piper5hz_subtask/piper5hz_subtask_dataset_builder.py
@@ -117,7 +117,8 @@ class Piper5HZ(tfds.core.GeneratorBasedBuilder):
                 ep =episode_path.split('/')[-2]
                 # img = Image.open(f'{path}/{ep}/exo/color_img_{6*i}.jpeg')
                 # img2 = Image.open(f'{path}/{ep}/wrist/color_img_{6*i}.jpeg')
-                print(data['state'].shape)
+                print(data.keys())
+                print(data['observation.state'].shape)
                 print(data['observation.images.table'].shape)
                 print(data['action'].shape)
                 exit()
